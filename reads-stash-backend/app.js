ff4558b16ca73eaaf4567f977d07882dbd82c82c
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-10 18:39:25 -0500

    Created routes folder and created userRoutes.js file inside. In userRoutes.js file I set up an express router and exported it as router and set up one mock route. Inside of app.js I imported the router as userRoutes variable and set up an app.use so that all of my user routes will start with /users.

--- reads-stash-backend/app.js
+++ reads-stash-backend/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
+
+app.use("/users", userRoutes);
 
 app.listen(3000, function () {
     console.log("App on port 3000");
