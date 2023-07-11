7491ff115b6279f1fc68cbadcf66f1c5ee044cc8
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 09:51:32 -0500

    Updated all routes in routes/users/userJournalsRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/users/userJournalsRoutes.js
+++ reads-stash-backend/routes/users/userJournalsRoutes.js
@@ -4,23 +4,25 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserJournals(req, res) {
-    return res.send("Mock get all user's journals request");
+    return res
+        .status(200)
+        .json({ msg: "Dummy get all user journals response" });
 });
 
 router.get("/:journal_id", function getOneUserJournal(req, res) {
-    return res.send("Mock get one user journal request");
+    return res.status(200).json({ msg: "get one user journal response" });
 });
 
 router.post("/", function createUserJournal(req, res) {
-    return res.send("Mock create user journal request");
+    return res.status(201).json({ msg: "create user journal response" });
 });
 
 router.patch("/:journal_id", function updateUserJournal(req, res) {
-    return res.send("Mock update user journal request");
+    return res.status(200).json({ msg: "Mock update user journal request" });
 });
 
 router.delete("/:journal_id", function deleteUserJournal(req, res) {
-    return res.send("Mock delete user journal request");
+    return res.status(200).json({ msg: "Mock delete user journal request" });
 });
 
 module.exports = router;
