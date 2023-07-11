3f61aa169cbdc5941fe32ca5fb5a9c976e51edd5
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 09:59:49 -0500

    Updated all routes in routes/users/userCollectionsRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/users/userCollectionsRoutes.js
+++ reads-stash-backend/routes/users/userCollectionsRoutes.js
@@ -4,23 +4,27 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserCollections(req, res) {
-    return res.send("Mock get all user's collections request");
+    return res
+        .status(200)
+        .json({ msg: "Mock get all user's collections request" });
 });
 
 router.get("/:collection_id", function getOneUserCollection(req, res) {
-    return res.send("Mock get one user collection request");
+    return res
+        .status(200)
+        .json({ msg: "Mock get one user collection request" });
 });
 
 router.post("/", function createUserCollection(req, res) {
-    return res.send("Mock create user collection request");
+    return res.status(201).json({ msg: "Mock create user collection request" });
 });
 
 router.patch("/:collection_id", function updateUserCollection(req, res) {
-    return res.send("Mock update user collection request");
+    return res.status(200).json({ msg: "Mock update user collection request" });
 });
 
 router.delete("/:collection_id", function deleteUserCollection(req, res) {
-    return res.send("Mock delete user collection request");
+    return res.status(200).json({ msg: "Mock delete user collection request" });
 });
 
 module.exports = router;
