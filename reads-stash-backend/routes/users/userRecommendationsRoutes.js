21b9d7552793ae20b88a1766f2e3b14359761dbd
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 09:44:57 -0500

    Updated all routes in routes/users/userRecommendationsRoutes.js to return a status code and a json response message.

--- reads-stash-backend/routes/users/userRecommendationsRoutes.js
+++ reads-stash-backend/routes/users/userRecommendationsRoutes.js
@@ -4,28 +4,38 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserRecommendations(req, res) {
-    return res.send("Mock get all user's recommendations request");
+    return res
+        .status(200)
+        .json({ msg: "Dummy get all user recommendations response" });
 });
 
 router.get("/:recommendation_id", function getOneUserRecommendation(req, res) {
-    return res.send("Mock get one user recommendation request");
+    return res
+        .status(200)
+        .json({ msg: "Dummy get one user recommendation response" });
 });
 
 router.post("/", function createUserRecommendation(req, res) {
-    return res.send("Mock create user recommendation request");
+    return res
+        .status(201)
+        .json({ msg: "Dummy created user recommendation response" });
 });
 
 router.patch(
     "/:recommendation_id",
     function updateUserRecommendation(req, res) {
-        return res.send("Mock update user recommendation request");
+        return res
+            .status(200)
+            .json({ msg: "Dummy updated user recommendation response" });
     }
 );
 
 router.delete(
     "/:recommendation_id",
     function deleteUserRecommendation(req, res) {
-        return res.send("Mock delete user recommendation request");
+        return res
+            .status(200)
+            .json({ msg: "Dummy deleted user recommendation response" });
     }
 );
 
