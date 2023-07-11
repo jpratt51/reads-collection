7d05db208f1dc9a92d6351e0958c42085e97186e
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 09:58:01 -0500

    Updated all routes in routes/users/userFollowedRoutes.js to return status codes and a json response.

--- reads-stash-backend/routes/users/userFollowedRoutes.js
+++ reads-stash-backend/routes/users/userFollowedRoutes.js
@@ -4,19 +4,27 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserFollowed(req, res) {
-    return res.send("Mock get all user's followed users request");
+    return res
+        .status(200)
+        .json({ msg: "Mock get all user's followed users request" });
 });
 
 router.get("/:followed_id", function getOneUserFollowed(req, res) {
-    return res.send("Mock get one user followed user request");
+    return res
+        .status(200)
+        .json({ msg: "Mock get one user followed user request" });
 });
 
 router.post("/", function createUserFollowed(req, res) {
-    return res.send("Mock create user followed user request");
+    return res
+        .status(201)
+        .json({ msg: "Mock create user followed user request" });
 });
 
 router.delete("/:followed_id", function deleteUserFollowed(req, res) {
-    return res.send("Mock delete user followed user request");
+    return res
+        .status(200)
+        .json({ msg: "Mock delete user followed user request" });
 });
 
 module.exports = router;
