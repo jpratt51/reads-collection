8970e97e0218feed65b5b38cc8bac30064153124
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:00:16 -0500

    Updated all routes for routes/users/userFollowersRoutes.js to have a third parameter 'next' and placed all return statements inside try catch statements so that the custome expressErrorHandler in app.js is handling all errors.

--- reads-stash-backend/routes/users/userFollowersRoutes.js
+++ reads-stash-backend/routes/users/userFollowersRoutes.js
@@ -3,14 +3,24 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllUserFollowers(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock get all user's followers request" });
+router.get("/", function getAllUserFollowers(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get all user's followers request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:follower_id", function getOneUserFollower(req, res) {
-    return res.status(200).json({ msg: "Mock get one user follower request" });
+router.get("/:follower_id", function getOneUserFollower(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get one user follower request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
