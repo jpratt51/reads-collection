c0f7d15970f681f4717de17b52b8d437dcea59f5
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:10:06 -0500

    Updated all routes in userBadgesRoutes.js to have a third parameter 'next' and placed all return statements inside of try catch statements so that the custom expressErrorHandler in app.js can handle all errors.

--- reads-stash-backend/routes/users/userBadgesRoutes.js
+++ reads-stash-backend/routes/users/userBadgesRoutes.js
@@ -3,20 +3,38 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllUserBadges(req, res) {
-    return res.status(200).json({ msg: "Mock get all user's badges request" });
+router.get("/", function getAllUserBadges(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get all user's badges request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:badge_id", function getOneUserBadge(req, res) {
-    return res.status(200).json({ msg: "Mock get one user badge request" });
+router.get("/:badge_id", function getOneUserBadge(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Mock get one user badge request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.post("/", function createUserBadge(req, res) {
-    return res.status(201).json({ msg: "Mock create user badge request" });
+router.post("/", function createUserBadge(req, res, next) {
+    try {
+        return res.status(201).json({ msg: "Mock create user badge request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.delete("/:badge_id", function deleteUserBadge(req, res) {
-    return res.status(200).json({ msg: "Mock delete user badge request" });
+router.delete("/:badge_id", function deleteUserBadge(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Mock delete user badge request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
