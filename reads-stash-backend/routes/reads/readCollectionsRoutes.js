35c45b3f1c9153dd7685aeb0740794d6f8f5dd44
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 10:05:29 -0500

    updated all routes in routes/reads/readCollectionsRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/reads/readCollectionsRoutes.js
+++ reads-stash-backend/routes/reads/readCollectionsRoutes.js
@@ -4,11 +4,11 @@ const express = require("express");
 const router = new express.Router();
 
 router.post("/", function createReadCollection(req, res) {
-    return res.send("Mock create read collection request");
+    return res.status(201).json({ msg: "Mock create read collection request" });
 });
 
 router.delete("/:collection_id", function deleteReadCollection(req, res) {
-    return res.send("Mock delete read collection request");
+    return res.status(200).json({ msg: "Mock delete read collection request" });
 });
 
 module.exports = router;
