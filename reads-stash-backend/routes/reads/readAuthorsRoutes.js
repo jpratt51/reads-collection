b1bd2414951f7e290177cd71cdc6bcb1a413fc3f
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 10:06:52 -0500

    Updated route in routes/reads/readAuthorsRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/reads/readAuthorsRoutes.js
+++ reads-stash-backend/routes/reads/readAuthorsRoutes.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = new express.Router();
 
 router.post("/", function createReadAuthor(req, res) {
-    return res.send("Mock create read author request");
+    return res.status(201).json({ msg: "Mock create read author request" });
 });
 
 module.exports = router;
