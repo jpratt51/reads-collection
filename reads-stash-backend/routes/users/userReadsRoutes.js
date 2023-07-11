2f0f478eb357f2ab63a2bca24a01190337d45ce2
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 09:48:17 -0500

    Updated all routes for routes/users/userReadsRoutes.js to return status code and a json response.

--- reads-stash-backend/routes/users/userReadsRoutes.js
+++ reads-stash-backend/routes/users/userReadsRoutes.js
@@ -4,19 +4,19 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserReads(req, res) {
-    return res.send("Mock get all user's reads request");
+    return res.status(200).json({ msg: "Dummy get all user reads response" });
 });
 
 router.get("/:users_reads_id", function getOneUserRead(req, res) {
-    return res.send("Mock get one user read request");
+    return res.status(200).json({ msg: "Dummy get one user read response" });
 });
 
 router.post("/", function createUserRead(req, res) {
-    return res.send("Mock create user read request");
+    return res.status(201).json({ msg: "Dummy created user read response" });
 });
 
 router.delete("/:collection_id", function deleteUserRead(req, res) {
-    return res.send("Mock delete user read request");
+    return res.status(200).json({ msg: "Dummy deleted user read response" });
 });
 
 module.exports = router;
