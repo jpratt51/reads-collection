5f0dadb590dfbbf7b0bf3420c1ca916609f6b862
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 10:03:33 -0500

    Updated all routes in routes/reads/readRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/reads/readRoutes.js
+++ reads-stash-backend/routes/reads/readRoutes.js
@@ -4,15 +4,15 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllReads(req, res) {
-    return res.send("Mock get all reads request");
+    return res.status(200).json({ msg: "Mock get all reads request" });
 });
 
 router.get("/:read_id", function getOneRead(req, res) {
-    return res.send("Mock get one read request");
+    return res.status(200).json({ msg: "Mock get one read request" });
 });
 
 router.post("/", function createRead(req, res) {
-    return res.send("Mock create read request");
+    return res.status(201).json({ msg: "Mock create read request" });
 });
 
 module.exports = router;
