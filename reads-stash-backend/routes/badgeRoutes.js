7ea8c9f741d3f42980cc9bb80eb3ff3750cd5f72
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 10:09:25 -0500

    Updated routes in routes/badgeRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/badgeRoutes.js
+++ reads-stash-backend/routes/badgeRoutes.js
@@ -4,11 +4,11 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllBadges(req, res) {
-    return res.send("Mock get all badges request");
+    return res.status(200).json({ msg: "Mock get all badges request" });
 });
 
 router.get("/:badge_id", function getOneBadge(req, res) {
-    return res.send("Mock get one badge request");
+    return res.status(200).json({ msg: "Mock get one badge request" });
 });
 
 module.exports = router;
