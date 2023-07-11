f1d0cae4aa4fa70a8f74d7c1e93b970db1bb6d5f
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:18:31 -0500

    Updated createAuthor route in routes/authorRoutes.js by adding athird parameter 'next' and placing the return statement inside a try catch statement so that all errors will be handled by the expressErrorHandler in app.js.

--- reads-stash-backend/routes/authorRoutes.js
+++ reads-stash-backend/routes/authorRoutes.js
@@ -3,8 +3,12 @@
 const express = require("express");
 const router = new express.Router();
 
-router.post("/", function createAuthor(req, res) {
-    return res.status(201).json({ msg: "Mock create author request" });
+router.post("/", function createAuthor(req, res, next) {
+    try {
+        return res.status(201).json({ msg: "Mock create author request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
