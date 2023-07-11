0f7132523a04b0b3c2fcbf676770842c2c0071cf
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:12:19 -0500

    Updated all routes for routes/reads/readRoutes.js by adding a third parameter 'next' and placing all return statements inside of try catch statements so that the customer expressErrorHandler in app.js will handle all errors.

--- reads-stash-backend/routes/reads/readRoutes.js
+++ reads-stash-backend/routes/reads/readRoutes.js
@@ -3,16 +3,28 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllReads(req, res) {
-    return res.status(200).json({ msg: "Mock get all reads request" });
+router.get("/", function getAllReads(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Mock get all reads request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:read_id", function getOneRead(req, res) {
-    return res.status(200).json({ msg: "Mock get one read request" });
+router.get("/:read_id", function getOneRead(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Mock get one read request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.post("/", function createRead(req, res) {
-    return res.status(201).json({ msg: "Mock create read request" });
+router.post("/", function createRead(req, res, next) {
+    try {
+        return res.status(201).json({ msg: "Mock create read request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
