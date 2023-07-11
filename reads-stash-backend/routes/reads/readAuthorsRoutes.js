665ae575f2826a2eae672165ec6d413465bf404a
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:16:00 -0500

    Updated createReadAuthor route in routes/reads/readAuthorsRoutes.js by adding third parameter 'next' and the return statement inside of a try catch statement so that the customer expressErrorHandler in app.js will handle all errors.

--- reads-stash-backend/routes/reads/readAuthorsRoutes.js
+++ reads-stash-backend/routes/reads/readAuthorsRoutes.js
@@ -3,8 +3,12 @@
 const express = require("express");
 const router = new express.Router();
 
-router.post("/", function createReadAuthor(req, res) {
-    return res.status(201).json({ msg: "Mock create read author request" });
+router.post("/", function createReadAuthor(req, res, next) {
+    try {
+        return res.status(201).json({ msg: "Mock create read author request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
