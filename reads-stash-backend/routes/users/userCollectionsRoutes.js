8b4db8e27dd65baf90e2f488047ebe89cccdc8bd
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:07:23 -0500

    Updated all routes for routes/users/userCollectionsRoutes.js to have a third parameter 'next' and placed all return statements inside of try catch statements so that the custom expressErrorHandler in app.js will handle all errors.

--- reads-stash-backend/routes/users/userCollectionsRoutes.js
+++ reads-stash-backend/routes/users/userCollectionsRoutes.js
@@ -3,28 +3,54 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllUserCollections(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock get all user's collections request" });
+router.get("/", function getAllUserCollections(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get all user's collections request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:collection_id", function getOneUserCollection(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock get one user collection request" });
+router.get("/:collection_id", function getOneUserCollection(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get one user collection request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.post("/", function createUserCollection(req, res) {
-    return res.status(201).json({ msg: "Mock create user collection request" });
+router.post("/", function createUserCollection(req, res, next) {
+    try {
+        return res
+            .status(201)
+            .json({ msg: "Mock create user collection request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.patch("/:collection_id", function updateUserCollection(req, res) {
-    return res.status(200).json({ msg: "Mock update user collection request" });
+router.patch("/:collection_id", function updateUserCollection(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock update user collection request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.delete("/:collection_id", function deleteUserCollection(req, res) {
-    return res.status(200).json({ msg: "Mock delete user collection request" });
+router.delete("/:collection_id", function deleteUserCollection(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock delete user collection request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
