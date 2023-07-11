e3f4f179740bcaac39f714384df767947a83a58a
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-10 20:02:04 -0500

    Created skeleton for user collection routes in userCollectionRoutes.js: getAllUserCollections, getOneUserCollection, createUserCollection, updateUserCollection, deleteUserCollection.

--- reads-stash-backend/routes/users/userCollectionRoutes.js
+++ reads-stash-backend/routes/users/userCollectionRoutes.js
@@ -3,4 +3,24 @@
 const express = require("express");
 const router = new express.Router();
 
+router.get("/", function getAllUserCollections(req, res) {
+    return res.send("Mock get all user's collections request");
+});
+
+router.get("/:collection_id", function getOneUserCollection(req, res) {
+    return res.send("Mock get one user collection request");
+});
+
+router.post("/", function createUserCollection(req, res) {
+    return res.send("Mock create user collection request");
+});
+
+router.patch("/:collection_id", function updateUserCollection(req, res) {
+    return res.send("Mock update user collection request");
+});
+
+router.delete("/:collection_id", function deleteUserCollection(req, res) {
+    return res.send("Mock delete user collection request");
+});
+
 module.exports = router;
