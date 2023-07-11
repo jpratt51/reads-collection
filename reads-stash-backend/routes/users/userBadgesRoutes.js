c206adde628dbf8aa58d6582f0e8da762e3da851
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 10:01:27 -0500

    Updated all routes in routes/users/userBadgesRoutes.js to return a status code and a json response.

--- reads-stash-backend/routes/users/userBadgesRoutes.js
+++ reads-stash-backend/routes/users/userBadgesRoutes.js
@@ -4,19 +4,19 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserBadges(req, res) {
-    return res.send("Mock get all user's badges request");
+    return res.status(200).json({ msg: "Mock get all user's badges request" });
 });
 
 router.get("/:badge_id", function getOneUserBadge(req, res) {
-    return res.send("Mock get one user badge request");
+    return res.status(200).json({ msg: "Mock get one user badge request" });
 });
 
 router.post("/", function createUserBadge(req, res) {
-    return res.send("Mock create user badge request");
+    return res.status(201).json({ msg: "Mock create user badge request" });
 });
 
 router.delete("/:badge_id", function deleteUserBadge(req, res) {
-    return res.send("Mock delete user badge request");
+    return res.status(200).json({ msg: "Mock delete user badge request" });
 });
 
 module.exports = router;
