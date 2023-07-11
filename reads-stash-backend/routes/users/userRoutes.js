70eb0004c094c3c34e107d400a4b8c5cb847bead
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 11:32:48 -0500

    Moved all route return statements into try catch blocks to handle errors in routes/users/userRoutes.js. Added next parameter to all routes.

--- reads-stash-backend/routes/users/userRoutes.js
+++ reads-stash-backend/routes/users/userRoutes.js
@@ -3,24 +3,44 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllUsers(req, res) {
-    return res.status(200).json({ msg: "Dummy all users result" });
+router.get("/", function getAllUsers(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Dummy all users result" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:user_id", function getOneUser(req, res) {
-    return res.status(200).json({ msg: "Dummy one user result" });
+router.get("/:user_id", function getOneUser(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Dummy one user result" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.post("/", function createUser(req, res) {
-    return res.status(201).json({ msg: "Dummy created user result" });
+router.post("/", function createUser(req, res, next) {
+    try {
+        return res.status(201).json({ msg: "Dummy created user result" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.patch("/:user_id", function updateUser(req, res) {
-    return res.status(200).json({ msg: "Dummy updated user result" });
+router.patch("/:user_id", function updateUser(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Dummy updated user result" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.delete("/:user_id", function deleteUser(req, res) {
-    return res.status(200).json({ msg: "Dummy deleted user result" });
+router.delete("/:user_id", function deleteUser(req, res, next) {
+    try {
+        return res.status(200).json({ msg: "Dummy deleted user result" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
