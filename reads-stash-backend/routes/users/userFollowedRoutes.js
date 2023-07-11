a70896e1cf21090cc982afd1a9e0d58a299e7848
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 12:03:14 -0500

    Updated all routes in routes/users/userFollowedRoutes.js by adding a third parameter 'next' and placing all return statements inside of try catch statements so that the customer expressErrorHandler in app.js is handling all errors.

--- reads-stash-backend/routes/users/userFollowedRoutes.js
+++ reads-stash-backend/routes/users/userFollowedRoutes.js
@@ -3,28 +3,44 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/", function getAllUserFollowed(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock get all user's followed users request" });
+router.get("/", function getAllUserFollowed(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get all user's followed users request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.get("/:followed_id", function getOneUserFollowed(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock get one user followed user request" });
+router.get("/:followed_id", function getOneUserFollowed(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock get one user followed user request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.post("/", function createUserFollowed(req, res) {
-    return res
-        .status(201)
-        .json({ msg: "Mock create user followed user request" });
+router.post("/", function createUserFollowed(req, res, next) {
+    try {
+        return res
+            .status(201)
+            .json({ msg: "Mock create user followed user request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
-router.delete("/:followed_id", function deleteUserFollowed(req, res) {
-    return res
-        .status(200)
-        .json({ msg: "Mock delete user followed user request" });
+router.delete("/:followed_id", function deleteUserFollowed(req, res, next) {
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Mock delete user followed user request" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
