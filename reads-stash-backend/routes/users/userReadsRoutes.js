74a0fa9250247c1f05d4e395e215c5c2b3b38adc
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 11:45:50 -0500

    Updated all routes in userReadsRoutes.js to have a third parameter 'next' and placed all return statements in a try catch to send any errors to the custom expressErrorHandler in app.js.

--- reads-stash-backend/routes/users/userReadsRoutes.js
+++ reads-stash-backend/routes/users/userReadsRoutes.js
@@ -4,19 +4,43 @@ const express = require("express");
 const router = new express.Router();
 
 router.get("/", function getAllUserReads(req, res) {
-    return res.status(200).json({ msg: "Dummy get all user reads response" });
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Dummy get all user reads response" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 router.get("/:users_reads_id", function getOneUserRead(req, res) {
-    return res.status(200).json({ msg: "Dummy get one user read response" });
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Dummy get one user read response" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 router.post("/", function createUserRead(req, res) {
-    return res.status(201).json({ msg: "Dummy created user read response" });
+    try {
+        return res
+            .status(201)
+            .json({ msg: "Dummy created user read response" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 router.delete("/:collection_id", function deleteUserRead(req, res) {
-    return res.status(200).json({ msg: "Dummy deleted user read response" });
+    try {
+        return res
+            .status(200)
+            .json({ msg: "Dummy deleted user read response" });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = router;
