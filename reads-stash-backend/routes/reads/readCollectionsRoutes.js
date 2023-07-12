5315c5f3f9c47eaef20c84c667c596b362547321
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-11 22:31:28 -0500

    Updated createReadCollection route function in routes/reads/readCollectionsRoutes.js to be an async function, destructured values from req.body, inserted those values into a db.query insert statement, and returned the result.

--- reads-stash-backend/routes/reads/readCollectionsRoutes.js
+++ reads-stash-backend/routes/reads/readCollectionsRoutes.js
@@ -4,11 +4,14 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../../db");
 
-router.post("/", function createReadCollection(req, res, next) {
+router.post("/", async function createReadCollection(req, res, next) {
     try {
-        return res
-            .status(201)
-            .json({ msg: "Mock create read collection request" });
+        const { read_id, collection_id } = req.body;
+        const results = await db.query(
+            "INSERT INTO reads_collections (read_id, collection_id) VALUES ($1, $2) RETURNING * ;",
+            [read_id, collection_id]
+        );
+        return res.status(201).json(results.rows);
     } catch (error) {
         return next(error);
     }
