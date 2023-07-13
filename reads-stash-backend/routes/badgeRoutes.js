1ee828b28e6eef36bb9fef3cafbb18d3a2f593b5
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-12 20:50:10 -0500

    Updated getAllBadges route function in routes/badgeRoutes.js to be an async function, to query the database for all badges and return the results.

--- reads-stash-backend/routes/badgeRoutes.js
+++ reads-stash-backend/routes/badgeRoutes.js
@@ -4,9 +4,10 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 
-router.get("/", function getAllBadges(req, res, next) {
+router.get("/", async function getAllBadges(req, res, next) {
     try {
-        return res.status(200).json({ msg: "Mock get all badges request" });
+        const results = await db.query("SELECT * FROM badges;");
+        return res.status(200).json(results.rows);
     } catch (error) {
         return next(error);
     }
