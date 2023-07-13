11457815edc146a22473982aa0b4ab95a900e74a
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-13 18:25:33 -0500

    Updated getOneUserCollection route function in routes/users/userCollectionsRoutes.js to be an async function, to destructure values from req.params, to make a query to the database using the values from req.params and return the resulting rows.

--- reads-stash-backend/routes/users/userCollectionsRoutes.js
+++ reads-stash-backend/routes/users/userCollectionsRoutes.js
@@ -21,11 +21,13 @@ router.get(
 
 router.get(
     "/:user_id/collections/:collection_id",
-    function getOneUserCollection(req, res, next) {
+    async function getOneUserCollection(req, res, next) {
         try {
-            return res
-                .status(200)
-                .json({ msg: "Mock get one user collection request" });
+            const { user_id, collection_id } = req.params;
+            const results = await db.query(
+                `SELECT * FROM collections WHERE id = ${collection_id} AND user_id = ${user_id};`
+            );
+            return res.status(200).json(results.rows);
         } catch (error) {
             return next(error);
         }
