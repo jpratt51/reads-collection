1e0d9aee89e271f21f1f893add3ad15b56961aaa
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-12 20:46:35 -0500

    Updated getAllReads route function in routes/reads/readRoutes.js to be an async function, to query the database for all reads and return the results.

--- reads-stash-backend/routes/reads/readRoutes.js
+++ reads-stash-backend/routes/reads/readRoutes.js
@@ -4,9 +4,10 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../../db");
 
-router.get("/", function getAllReads(req, res, next) {
+router.get("/", async function getAllReads(req, res, next) {
     try {
-        return res.status(200).json({ msg: "Mock get all reads request" });
+        const results = await db.query("SELECT * FROM reads;");
+        return res.status(200).json(results.rows);
     } catch (error) {
         return next(error);
     }
