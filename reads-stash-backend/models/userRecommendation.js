cbfbd169208b70822134b1c408029396fe50a7c0
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-15 14:00:47 -0500

    Updated models/userRecommendation.js variable names from lower snake case to camel case.

--- reads-stash-backend/models/userRecommendation.js
+++ reads-stash-backend/models/userRecommendation.js
@@ -3,25 +3,25 @@
 const db = require("../db");
 
 class Recommendation {
-    constructor(id, recommendation, sender_id, receiver_id) {
+    constructor(id, recommendation, senderId, receiverId) {
         this.id = id;
         this.recommendation = recommendation;
-        this.sender_id = sender_id;
-        this.receiver_id = receiver_id;
+        this.sender_id = senderId;
+        this.receiver_id = receiverId;
     }
 
-    static async getAll(user_id) {
+    static async getAll(userId) {
         const results = await db.query(
             `SELECT * FROM recommendations WHERE receiver_id = $1 OR sender_id = $1;`,
-            [user_id]
+            [userId]
         );
         const recommendations = results.rows.map(
             (r) =>
                 new Recommendation(
                     r.id,
                     r.recommendation,
-                    r.sender_id,
-                    r.receiver_id
+                    r.senderId,
+                    r.receiverId
                 )
         );
         return recommendations;
