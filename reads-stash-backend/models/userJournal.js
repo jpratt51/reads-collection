aa595a9799309fd28ae45c4e251f49fa4f8eec5e
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-15 13:59:15 -0500

    Updated models/userJournal.js variable names from lower snake case to camel case.

--- reads-stash-backend/models/userJournal.js
+++ reads-stash-backend/models/userJournal.js
@@ -3,21 +3,21 @@
 const db = require("../db");
 
 class UserJournal {
-    constructor(id, title, date, text, user_id) {
+    constructor(id, title, date, text, userId) {
         this.id = id;
         this.title = title;
         this.date = date;
         this.text = text;
-        this.user_id = user_id;
+        this.user_id = userId;
     }
 
-    static async getAll(user_id) {
+    static async getAll(userId) {
         const results = await db.query(
             `SELECT * FROM journals WHERE user_id = $1;`,
-            [user_id]
+            [userId]
         );
         const journals = results.rows.map(
-            (r) => new UserJournal(r.id, r.title, r.date, r.text, r.user_id)
+            (r) => new UserJournal(r.id, r.title, r.date, r.text, r.userId)
         );
         return journals;
     }
