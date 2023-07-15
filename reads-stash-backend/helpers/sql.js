2ed78785f04c7eceda2dd73bc34c2557530cb2e6
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-14 22:06:10 -0500

    Created folder helpers and added file sql.js. Created function dataToSql which takes in an object of user inputs and formats it into valid sql to be used for a sql update query. Returns those values as an object.

---
+++ reads-stash-backend/helpers/sql.js
@@ -0,0 +1,24 @@
+// Take in data and transform it to sql for update query
+
+// Example: data = {"username" : "bigDeal100", "fname" : "Big", "lname" : "Deal"}
+// returns {
+// columns: '"username"=$1, "fname"=$2, "lname"=$3',
+// values: ["bigDeal100", "Big", "Deal"]
+// }
+
+function dataToSql(data) {
+    const keys = Object.keys(data);
+    if (keys.length === 0) return { error: "No data" };
+
+    const columns = keys.map((name, idx) => `"${name}" = $${idx + 1}`);
+    const values = Object.values(data);
+
+    return {
+        columns: columns.join(", "),
+        values: values,
+    };
+}
+
+const data = { username: "bigDeal100", fname: "Big", lname: "Deal" };
+
+dataToSql(data);
