28f9319712bbfeeac99b7f9dccaed82312fe645a
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-15 14:02:44 -0500

    Updated variable names in routes/reads/readCollectionsRoutes.js from lower snake case to camel case.

--- reads-stash-backend/routes/reads/readCollectionsRoutes.js
+++ reads-stash-backend/routes/reads/readCollectionsRoutes.js
@@ -5,13 +5,13 @@ const router = new express.Router();
 const db = require("../../db");
 
 router.post(
-    "/:read_id/collections",
+    "/:readId/collections",
     async function createReadCollection(req, res, next) {
         try {
-            const { read_id, collection_id } = req.body;
+            const { readId, collectionId } = req.body;
             const results = await db.query(
                 "INSERT INTO reads_collections (read_id, collection_id) VALUES ($1, $2) RETURNING * ;",
-                [read_id, collection_id]
+                [readId, collectionId]
             );
             return res.status(201).json(results.rows);
         } catch (error) {
@@ -21,19 +21,17 @@ router.post(
 );
 
 router.delete(
-    "/:read_id/collections/:collection_id",
+    "/:readId/collections/:collectionId",
     async function deleteReadCollection(req, res, next) {
         try {
-            const { read_id, collection_id } = req.params;
+            const { readId, collectionId } = req.params;
             await db.query(
                 "DELETE FROM reads_collections WHERE read_id = $1 AND collection_id = $2;",
-                [read_id, collection_id]
+                [readId, collectionId]
             );
-            return res
-                .status(200)
-                .json({
-                    msg: `Deleted read ${read_id} from collection ${collection_id}`,
-                });
+            return res.status(200).json({
+                msg: `Deleted read ${readId} from collection ${collectionId}`,
+            });
         } catch (error) {
             return next(error);
         }
