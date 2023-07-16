f84559da56f043cee69f9c0b30cbed7014f0155b
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 17:29:09 -0500

    Created file routes/auth.js, setup basic skeleton for file including setting to use strict mode, importing express, creating new express router, importing db and exporting router. Setup skeleton for first route in auth.js with the url of /register.

---
+++ reads-stash-backend/routes/auth.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const router = new express.Router();
+const db = require("../db");
+
+route.post("/register", async (req, res, next) => {
+    try {
+    } catch (error) {
+        return next(e);
+    }
+});
+
+module.exports = router;
