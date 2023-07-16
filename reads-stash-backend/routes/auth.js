ec27cf29e5d994471b28daa96fa9d7717e0b7ad8
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 17:30:11 -0500

    Updated function in routes/auth.js from an arrow function to a named function registerUser.

--- reads-stash-backend/routes/auth.js
+++ reads-stash-backend/routes/auth.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 
-route.post("/register", async (req, res, next) => {
+route.post("/register", async function registerUser(req, res, next) {
     try {
     } catch (error) {
         return next(e);
