5f3d2e5aeaaa7d89edf23b1415f2bbe13df62cc9
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 17:51:03 -0500

    Setup basic functionality for registerUser in routes/auth by copying and pasting in the guts of the createUser route function from routes/users/users. Imported User class into routes/auth, called bcrypt.hash on the user password input from req.body with a bcrypt work factor of 12, saved the hashedPw to a variable, passed the hashed password into the User.create method instead of the string password, and threw an ExpressError with status code 400 if no username or password were sent in req.body.

--- reads-stash-backend/routes/auth.js
+++ reads-stash-backend/routes/auth.js
@@ -4,9 +4,17 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const User = require("../models/users/user");
 
 route.post("/register", async function registerUser(req, res, next) {
     try {
+        const { username, fname, lname, email, password } = req.body;
+        if (!username || !password) {
+            throw new ExpressError("Username and password required", 400);
+        }
+        const hashedPw = await bcrypt.hash(password, 12);
+        const user = await User.create(username, fname, lname, email, hashedPw);
+        return res.status(201).json(user);
     } catch (error) {
         return next(e);
     }
