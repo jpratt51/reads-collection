c125911f1809b09a66ceeefc30d54818c130dc41
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 20:53:44 -0500

    Updated routes/users/reads by importing ensureLoggedIn middleware function from middleware/auth and adding it to all users reads routes.

--- reads-stash-backend/routes/users/reads.js
+++ reads-stash-backend/routes/users/reads.js
@@ -4,19 +4,25 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../../db");
 const UserRead = require("../../models/users/read");
+const { ensureLoggedIn } = require("../../middleware/auth");
 
-router.get("/:userId/reads", async function getAllUserReads(req, res, next) {
-    try {
-        const { userId } = req.params;
-        let userReads = await UserRead.getAll(userId);
-        return res.status(200).json(userReads);
-    } catch (error) {
-        return next(error);
+router.get(
+    "/:userId/reads",
+    ensureLoggedIn,
+    async function getAllUserReads(req, res, next) {
+        try {
+            const { userId } = req.params;
+            let userReads = await UserRead.getAll(userId);
+            return res.status(200).json(userReads);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 router.get(
     "/:userId/reads/:usersReadsId",
+    ensureLoggedIn,
     async function getOneUserRead(req, res, next) {
         try {
             const { userId, usersReadsId } = req.params;
@@ -28,19 +34,24 @@ router.get(
     }
 );
 
-router.post("/:userId/reads", async function createUserRead(req, res, next) {
-    try {
-        const { userId } = req.params;
-        const { readId } = req.body;
-        const userRead = await UserRead.create(userId, readId);
-        return res.status(201).json(userRead);
-    } catch (error) {
-        return next(error);
+router.post(
+    "/:userId/reads",
+    ensureLoggedIn,
+    async function createUserRead(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const { readId } = req.body;
+            const userRead = await UserRead.create(userId, readId);
+            return res.status(201).json(userRead);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 router.delete(
     "/:userId/reads/:usersReadsId",
+    ensureLoggedIn,
     async function deleteUserRead(req, res, next) {
         try {
             const { userId, usersReadsId } = req.params;
