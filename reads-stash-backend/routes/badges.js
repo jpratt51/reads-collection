0ceeca0b5a898c86641dea396c2b16c4092f39d3
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 21:04:28 -0500

    Updated routes/badges by importing ensureLoggedIn middleware function from middleware/auth and adding it to every badge route.

--- reads-stash-backend/routes/badges.js
+++ reads-stash-backend/routes/badges.js
@@ -4,8 +4,9 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 const Badge = require("../models/badge");
+const { ensureLoggedIn } = require("../middleware/auth");
 
-router.get("/", async function getAllBadges(req, res, next) {
+router.get("/", ensureLoggedIn, async function getAllBadges(req, res, next) {
     try {
         let badges = await Badge.getAll();
         return res.status(200).json(badges);
@@ -14,14 +15,18 @@ router.get("/", async function getAllBadges(req, res, next) {
     }
 });
 
-router.get("/:badgeId", async function getOneBadge(req, res, next) {
-    try {
-        const { badgeId } = req.params;
-        let badge = await Badge.getById(badgeId);
-        return res.status(200).json(badge);
-    } catch (error) {
-        return next(error);
+router.get(
+    "/:badgeId",
+    ensureLoggedIn,
+    async function getOneBadge(req, res, next) {
+        try {
+            const { badgeId } = req.params;
+            let badge = await Badge.getById(badgeId);
+            return res.status(200).json(badge);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 module.exports = router;
