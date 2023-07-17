7d5acba03a79e88f68cc31e345243c5d4dee2a87
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 21:02:21 -0500

    Updated routes/users/badges by importing ensureLoggedIn middleware from middleware/auth and adding it to every users badges route.

--- reads-stash-backend/routes/users/badges.js
+++ reads-stash-backend/routes/users/badges.js
@@ -4,19 +4,25 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../../db");
 const UserBadge = require("../../models/users/badge");
+const { ensureLoggedIn } = require("../../middleware/auth");
 
-router.get("/:userId/badges", async function getAllUserBadges(req, res, next) {
-    try {
-        const { userId } = req.params;
-        const userBadges = await UserBadge.getAll(userId);
-        return res.status(200).json(userBadges);
-    } catch (error) {
-        return next(error);
+router.get(
+    "/:userId/badges",
+    ensureLoggedIn,
+    async function getAllUserBadges(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const userBadges = await UserBadge.getAll(userId);
+            return res.status(200).json(userBadges);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 router.get(
     "/:userId/badges/:badgeId",
+    ensureLoggedIn,
     async function getOneUserBadge(req, res, next) {
         try {
             const { userId, badgeId } = req.params;
@@ -28,18 +34,23 @@ router.get(
     }
 );
 
-router.post("/:userId/badges", async function createUserBadge(req, res, next) {
-    try {
-        const { userId, badgeId } = req.body;
-        const badge = await UserBadge.create(userId, badgeId);
-        return res.status(201).json(badge);
-    } catch (error) {
-        return next(error);
+router.post(
+    "/:userId/badges",
+    ensureLoggedIn,
+    async function createUserBadge(req, res, next) {
+        try {
+            const { userId, badgeId } = req.body;
+            const badge = await UserBadge.create(userId, badgeId);
+            return res.status(201).json(badge);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 router.delete(
     "/:userId/badges/:userBadgeId",
+    ensureLoggedIn,
     async function deleteUserBadge(req, res, next) {
         try {
             const { userId, userBadgeId } = req.params;
