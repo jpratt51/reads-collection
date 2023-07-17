9e1ff0ddd82aef65b620e85e4c4c2b200dac16da
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-16 20:50:02 -0500

    Added ensureLoggedIn middleware to all user route functions in routes/users/users.

--- reads-stash-backend/routes/users/users.js
+++ reads-stash-backend/routes/users/users.js
@@ -6,7 +6,7 @@ const db = require("../../db");
 const User = require("../../models/users/user");
 const { ensureLoggedIn } = require("../../middleware/auth");
 
-router.get("/", async function getAllUsers(req, res, next) {
+router.get("/", ensureLoggedIn, async function getAllUsers(req, res, next) {
     try {
         let users = await User.getAll();
         return res.status(200).json(users);
@@ -29,41 +29,49 @@ router.get(
     }
 );
 
-router.post("/", async function createUser(req, res, next) {
-    try {
-        const { username, fname, lname, email, password } = req.body;
-        const user = await User.create(username, fname, lname, email, password);
-        return res.status(201).json(user);
-    } catch (error) {
-        return next(error);
-    }
-});
+// router.post("/", async function createUser(req, res, next) {
+//     try {
+//         const { username, fname, lname, email, password } = req.body;
+//         const user = await User.create(username, fname, lname, email, password);
+//         return res.status(201).json(user);
+//     } catch (error) {
+//         return next(error);
+//     }
+// });
 
-router.patch("/:userId", async function updateUser(req, res, next) {
-    try {
-        const { userId } = req.params;
-        const inputs = req.body;
-        const user = await User.getById(userId);
-        inputs.username ? (user.username = inputs.username) : null;
-        inputs.fname ? (user.fname = inputs.fname) : null;
-        inputs.lname ? (user.lname = inputs.lname) : null;
-        inputs.email ? (user.email = inputs.email) : null;
-        await user.update();
-        return res.status(200).json(user);
-    } catch (error) {
-        return next(error);
+router.patch(
+    "/:userId",
+    ensureLoggedIn,
+    async function updateUser(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const inputs = req.body;
+            const user = await User.getById(userId);
+            inputs.username ? (user.username = inputs.username) : null;
+            inputs.fname ? (user.fname = inputs.fname) : null;
+            inputs.lname ? (user.lname = inputs.lname) : null;
+            inputs.email ? (user.email = inputs.email) : null;
+            await user.update();
+            return res.status(200).json(user);
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
-router.delete("/:userId", async function deleteUser(req, res, next) {
-    try {
-        const { userId } = req.params;
-        const user = await User.getById(userId);
-        await user.delete();
-        return res.status(200).json({ msg: `Deleted user ${userId}` });
-    } catch (error) {
-        return next(error);
+router.delete(
+    "/:userId",
+    ensureLoggedIn,
+    async function deleteUser(req, res, next) {
+        try {
+            const { userId } = req.params;
+            const user = await User.getById(userId);
+            await user.delete();
+            return res.status(200).json({ msg: `Deleted user ${userId}` });
+        } catch (error) {
+            return next(error);
+        }
     }
-});
+);
 
 module.exports = router;
