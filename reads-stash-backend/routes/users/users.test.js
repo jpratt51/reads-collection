c5840bbb683c1f7661f6cd3c3d5b9fb293563199
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-18 11:41:27 -0500

    Updated routes/users users.test.js by requiring bcrypt, jsonwebtoken and the secret key from the config file. Created beforeAll async function which creates a hashed password using bcrypt and passes into a db sql insertion query to create a new user with the hashed password. It then creates a payload from the query's result grabbing the user's username and id. Then it passes that payload to jwt.sign with the secret key and stores the resulting token to the global testUserToken to be used as validation for the rest of my tests that require a logged in user. Created test function for my /register route to see if it returns a token when sent the proper data. Created an empty afterEach function and a afterAll function which deletes all users and ends the test with db.end.

--- reads-stash-backend/routes/users/users.test.js
+++ reads-stash-backend/routes/users/users.test.js
@@ -3,5 +3,42 @@
 process.env.NODE_ENV = "test";
 
 const request = require("supertest");
+
 const app = require("../../app");
 const db = require("../../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../config");
+
+let testUserToken;
+
+beforeAll(async () => {
+    const hashedPassword = await bcrypt.hash("secret", 1);
+    const result = await db.query(
+        `INSERT INTO users (username, fname, lname, email, password) VALUES ('test', 'tfn', 'tln', 'test@email.com', $1) RETURNING username, id`,
+        [hashedPassword]
+    );
+    const testUser = { username: result.username, id: result.id };
+    testUserToken = jwt.sign(testUser, SECRET_KEY);
+});
+
+describe("POST /register", function () {
+    test("returns token", async function () {
+        const response = await request(app).post("/api/auth/register").send({
+            username: "test2",
+            fname: "t2fn",
+            lname: "t2ln",
+            email: "t2@email.com",
+            password: "password",
+        });
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({ token: expect.any(String) });
+    });
+});
+
+afterEach(async () => {});
+
+afterAll(async () => {
+    await db.query("DELETE FROM users;");
+    await db.end();
+});
