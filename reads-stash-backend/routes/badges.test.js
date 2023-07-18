5b485e8215be56a502b5f2e0c09c7b87143a9b85
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-18 12:42:58 -0500

    Created file routes/badges.test.js. Set to use strict mode, set process.env.NODE_ENV to test, required supertest, app, db, bcrypt, jsonwebtoken, and secret key.

---
+++ reads-stash-backend/routes/badges.test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+
+const app = require("../app");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
