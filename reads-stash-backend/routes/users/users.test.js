097708a44589205fa329437f217746147313b0c9
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-18 15:03:33 -0500

    Created file routes/users.test.js and added in boiler plate setup for tests including all the dependencies I used in badges.test.js with updated paths and my beforeAll and afterAll functions.

---
+++ reads-stash-backend/routes/users/users.test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+process.env.NODE_ENV = "test";
+
+const request = require("supertest");
+
+const app = require("../../app");
+const db = require("../../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../config");
+
+let testUserToken;
+
+beforeAll(async () => {
+    const hashedPassword = await bcrypt.hash("secret", 1);
+    const res = await db.query(
+        `INSERT INTO users (username, fname, lname, email, password) VALUES ('test1', 'tfn', 'tln', 'test@email.com', $1) RETURNING username, id`,
+        [hashedPassword]
+    );
+    const testUser = { username: res.username, id: res.id };
+    testUserToken = jwt.sign(testUser, SECRET_KEY);
+});
+
+afterAll(async () => {
+    await db.query("DELETE FROM users;");
+    await db.end();
+});
