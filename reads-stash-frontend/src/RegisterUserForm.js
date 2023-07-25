2cb3b259a3dac3690074abeb8255cc2a37427eb4
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-25 13:45:45 -0500

    Updated RegisterUserForm by adding an INITIAL_STATE variable to set the state for all of my inputs to an empty string for my RegisterUserForm. Changed the piece of state from username to formData since I will have multiple inputs. Changed the useState for to the INITIAL_STATE object rather than an empty string. Updated the handleChange function by destructuring name and value from e.target and programmatically updating setFormData state to whatever is already in there but changing the key's value that matches the e.target.name property to e.target.value. Created handlesubmit function which calls e.preventDefault so the form doesn't clear out automatically every time it is submitted since we want to control this component with state, then called setFormData passing in the INITIAL_STATE object to reset the form the react way. Added inputs to the form: fname, lname, email, password and updated their ids, names and placeholders accordingly. Changed the value property on the username input from username to formData.username and followed this pattern for every input. Set up a label with an htmlFor property for every input and set the names to match the input. Added an onClick passing in handleSubmit to the button for the form.

--- reads-stash-frontend/src/RegisterUserForm.js
+++ reads-stash-frontend/src/RegisterUserForm.js
@@ -1,19 +1,71 @@
 import React, { useState } from "react";
 
 const RegisterUserForm = () => {
-    const [username, setUsername] = useState("");
+    const INITIAL_STATE = {
+        username: "",
+        fname: "",
+    };
+    const [formData, setFormData] = useState(INITIAL_STATE);
     const handleChange = (e) => {
-        setUsername(e.target.value);
+        const { name, value } = e.target;
+        setFormData((data) => ({
+            ...data,
+            [name]: value,
+        }));
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // const { username, fname } = formData;
+        setFormData(INITIAL_STATE);
     };
     return (
         <form>
+            <label htmlFor="username">Username</label>
             <input
+                id="username"
                 type="text"
+                name="username"
                 placeholder="username"
-                value={username}
+                value={formData.username}
                 onChange={handleChange}
             />
-            <button>Signup</button>
+            <label htmlFor="fname">First Name</label>
+            <input
+                id="fname"
+                type="text"
+                name="fname"
+                placeholder="first name"
+                value={formData.fname}
+                onChange={handleChange}
+            />
+            <label htmlFor="lname">Last Name</label>
+            <input
+                id="lname"
+                type="text"
+                name="lname"
+                placeholder="last name"
+                value={formData.fname}
+                onChange={handleChange}
+            />
+            <label htmlFor="email">Email</label>
+            <input
+                id="email"
+                type="text"
+                name="email"
+                placeholder="email"
+                value={formData.fname}
+                onChange={handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                id="password"
+                type="text"
+                name="password"
+                placeholder="password"
+                value={formData.fname}
+                onChange={handleChange}
+            />
+            <button onClick={handleSubmit}>Signup</button>
         </form>
     );
 };
