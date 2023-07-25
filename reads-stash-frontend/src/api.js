f1b39127858a52b588ece5ad52b5671f36d2d1f6
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-25 10:28:07 -0500

    Created file reads-stash-frontend/src/api.js and created class called ReadsStashApi which will be the connection between the frontend client side app and the backend api. Sends a request static async function takes in the endpoint, data and method which defaults to get and sends a request to the backend api with a token collected from localStorage if it is there. Params will either be the data sent in or an empty object depending on if the request is a get request or not. Then an axios request is made to the backend api with the url, method, data, params, and headers and the data from that axios call is returned. If there is an error, return that error instead. Exported the ReadsStashApi.

---
+++ reads-stash-frontend/src/api.js
@@ -0,0 +1,110 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+class ReadsStashApi {
+    static token;
+
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("API Call:", endpoint, data, method);
+
+        let { token } = JSON.parse(localStorage.getItem("user")) || "";
+
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = { _token: `${token}` };
+        const params = method === "get" ? data : {};
+
+        try {
+            return (await axios({ url, method, data, params, headers })).data;
+        } catch (err) {
+            console.error("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        }
+    }
+
+    // static async getAllCompanies() {
+    //     let res = await this.request(`companies`);
+    //     return res.companies;
+    // }
+
+    // static async getCompany(handle) {
+    //     let res = await this.request(`companies/${handle}`);
+    //     return res.company;
+    // }
+
+    // static async findAllCompanies(name = "", minEmployees, maxEmployees) {
+    //     let data;
+    //     name === ""
+    //         ? (data = { minEmployees, maxEmployees })
+    //         : (data = { name, minEmployees, maxEmployees });
+    //     let res = await this.request(data);
+    //     return res.companies;
+    // }
+
+    // static async findAllJobs(title = "", minSalary = 0, hasEquity = false) {
+    //     let data;
+    //     title === ""
+    //         ? (data = { minSalary, hasEquity })
+    //         : (data = { title, minSalary, hasEquity });
+    //     let res = await this.request("jobs", data);
+    //     return res.jobs;
+    // }
+
+    // static async register(username, password, firstName, lastName, email) {
+    //     let res = await this.request(
+    //         `auth/register`,
+    //         {
+    //             username,
+    //             password,
+    //             firstName,
+    //             lastName,
+    //             email,
+    //         },
+    //         "post"
+    //     );
+    //     return res.token;
+    // }
+
+    // static async login(username, password) {
+    //     let res = await this.request(
+    //         `auth/token`,
+    //         {
+    //             username,
+    //             password,
+    //         },
+    //         "post"
+    //     );
+    //     return res.token;
+    // }
+
+    // static async updateUser(password, firstName, lastName, email) {
+    //     const user = JSON.parse(localStorage.getItem("user"));
+    //     let data = {};
+    //     if (password) data["password"] = password;
+    //     if (firstName) data["firstName"] = firstName;
+    //     if (lastName) data["lastName"] = lastName;
+    //     if (email) data["email"] = email;
+    //     let res = await this.request(`users/${user.username}`, data, "patch");
+    //     return res;
+    // }
+
+    // static async getUserData() {
+    //     const user = JSON.parse(localStorage.getItem("user")) || {
+    //         username: "test2",
+    //     };
+    //     let res = await this.request(`users/${user.username}`);
+    //     return res;
+    // }
+
+    // static async apply(username, jobId) {
+    //     let res = await this.request(
+    //         `users/${username}/jobs/${jobId}`,
+    //         null,
+    //         "post"
+    //     );
+    //     return res;
+    // }
+}
+
+export default ReadsStashApi;
