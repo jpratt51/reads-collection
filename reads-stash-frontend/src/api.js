37de6753a744a601acd121d224d5566ef39dc9f6
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-07-25 20:36:25 -0500

    Updated key name for useLogin custom hook from token to user.

--- reads-stash-frontend/src/api.js
+++ reads-stash-frontend/src/api.js
@@ -10,7 +10,7 @@ class ReadsStashApi {
         console.log("API Call:", endpoint, data, method);
         let token;
         try {
-            token = JSON.parse(localStorage.getItem("token")) || "";
+            token = JSON.parse(localStorage.getItem("user")) || "";
         } catch {
             token = "";
         }
