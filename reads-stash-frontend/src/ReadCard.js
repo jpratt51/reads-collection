51852845cb32375870ed0256fac15fb58da469bc
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-09-01 22:08:56 -0500

    Updated ReadCard.js by importing useState and useEffect, added a piece of state for readDescription, added a function called truncate which takes in a text and a max length and returns either the full text or a truncated text, added a useEffect which changes the button text if the description is longer than 200 characters and is dependent on props.description, added a function toggleDescriptionLength which changes the length of the displayed description between a max length of 200 and 2000 characters, changed the title h1 in the return statement to display only the title and nothing else, changed the the message for the paragraph displaying authors, rearranged the paragraph tags displaying the read information, changed the description paragraph to display the readDescription piece of state info and include a button to toggle the readDescription text length on click and added target and rel attributes to the infoLink Link component to open a new tab when the user clicks on it.

--- reads-stash-frontend/src/ReadCard.js
+++ reads-stash-frontend/src/ReadCard.js
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const ReadCard = (props) => {
+    const [readDescription, setReadDescription] = useState("");
+
+    function truncate(text, maxLength) {
+        if (text.length > maxLength) {
+            text = text.substring(0, maxLength) + " ...";
+        }
+        return text;
+    }
+
+    useEffect(() => {
+        if (props.description.length > 200) {
+            setReadDescription({
+                descriptionText: truncate(props.description, 200),
+                buttonText: "show more",
+            });
+        } else {
+            setReadDescription({
+                descriptionText: truncate(props.description, 200),
+                buttonText: "",
+            });
+        }
+    }, [props.description]);
+
+    const toggleDescriptionLength = () => {
+        if (readDescription.descriptionText.length === 204) {
+            setReadDescription({
+                descriptionText: truncate(props.description, 2000),
+                buttonText: "show less",
+            });
+        } else {
+            setReadDescription({
+                descriptionText: truncate(props.description, 200),
+                buttonText: "show more",
+            });
+        }
+    };
+
     return (
         <div>
-            <h1>Title: {props.title}</h1>
-            <p>ISBN: {props.isbn}</p>
-            <p>Date Published: {props.publishedDate}</p>
-            <p>Description: {props.description}</p>
-            <p>Page Count: {props.pageCount}</p>
-            <p>Print Type: {props.printType}</p>
+            <h1>{props.title}</h1>
+            <p>By {props.authors}</p>
             <Link to={`/read/${props.isbn}`} state={{ props: props }}>
                 <img src={props.thumbnail} alt="book cover" />
             </Link>
-            <p>Info URL: {props.infoLink}</p>
-            <p>Authors: {props.authors}</p>
+            <p>
+                Description: {readDescription.descriptionText}
+                {props.description.length > 200 ? (
+                    <button onClick={toggleDescriptionLength}>
+                        {readDescription.buttonText}
+                    </button>
+                ) : null}
+            </p>
+            <p>Date Published: {props.publishedDate}</p>
+            <p>Page Count: {props.pageCount}</p>
+            <p>Print Type: {props.printType}</p>
+            <Link to={props.infoLink} target="_blank" rel="noopener noreferrer">
+                <p>Google Books link</p>
+            </Link>
+            <p>ISBN: {props.isbn}</p>
         </div>
     );
 };
