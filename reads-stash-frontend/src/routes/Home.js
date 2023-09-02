a1af45cda59336b8ba6fdc4f586f3b6280b64267
Joel Pratt <joelpratt@Ashtons-MBP-2.lan>
2023-09-01 19:50:26 -0500

    Updated routes/Home.js by wrapping the return statement in a div and added two links, one to the login page and one to the registration page, as well as an h2 message.

--- reads-stash-frontend/src/routes/Home.js
+++ reads-stash-frontend/src/routes/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return <h1>Welcome to reads stash!</h1>;
+    return (
+        <div>
+            <h1>Welcome to reads stash!</h1>
+            <h2>Sign in or register below!</h2>
+            <Link to="/auth/login">Sign In</Link>
+            <Link to="/auth/register">Register</Link>
+        </div>
+    );
 };
 
 export default Home;
